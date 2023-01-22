19d9d2e81356732cf5e333d746d9720345460f84
milenavsmilka <milena.milena16@onet.pl>
2023-01-22 20:40:21 +0100

    zmiana nazw pól

--- app/src/main/java/com/example/systemymobilneprojekt/LoginActivity.java
+++ app/src/main/java/com/example/systemymobilneprojekt/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.example.systemymobilneprojekt;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
@@ -8,22 +7,14 @@ import android.app.Activity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Intent;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.systemymobilneprojekt.api.DataFromApi;
-import com.example.systemymobilneprojekt.db.tables.Client;
-import com.example.systemymobilneprojekt.db.DatabaseOperations;
-
-import java.util.List;
 /*
 public class MainActivity extends AppCompatActivity {
     EditText username = (EditText)findViewById(R.id.usernameInput);
@@ -59,10 +50,10 @@ public class MainActivity extends AppCompatActivity {
 */
 
 public class LoginActivity extends Activity {
-    Button b1,b2;
-    EditText ed1,ed2;
+    Button logButton, gotoYTButton;
+    EditText userEditText, passwordEditText;
 
-    TextView tx1;
+    TextView errorTextView;
     int counter = 3;
 
     @Override
@@ -70,32 +61,31 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O)
-        {
-            NotificationChannel channel = new NotificationChannel("Witaj w pizzerii!","Witaj w pizzerii!",NotificationManager.IMPORTANCE_DEFAULT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("Witaj w pizzerii!", "Witaj w pizzerii!", NotificationManager.IMPORTANCE_DEFAULT);
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
         }
-        b1 = (Button)findViewById(R.id.loginButton);
-        ed1 = (EditText)findViewById(R.id.usernameInput);
-        ed2 = (EditText)findViewById(R.id.passwordInput);
-        b2 = (Button)findViewById(R.id.rickRollButton);
-        tx1 = (TextView)findViewById(R.id.errorText);
-        tx1.setVisibility(View.GONE);
+        logButton = (Button) findViewById(R.id.loginButton);
+        userEditText = (EditText) findViewById(R.id.usernameInput);
+        passwordEditText = (EditText) findViewById(R.id.passwordInput);
+        gotoYTButton = (Button) findViewById(R.id.rickRollButton);
+        errorTextView = (TextView) findViewById(R.id.errorText);
+        errorTextView.setVisibility(View.GONE);
 
-        b1.setOnClickListener(new View.OnClickListener() {
+        logButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!ed1.getText().toString().equals("") &&
-                        !ed2.getText().toString().equals("")) {
+                if (!userEditText.getText().toString().equals("") &&
+                        !passwordEditText.getText().toString().equals("")) {
                     //tu jest logowanie
                     sendLoginNotification();
-                }else{
-                    Toast.makeText(getApplicationContext(), "Nie podano danych",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Nie podano danych", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-        b2.setOnClickListener(new View.OnClickListener() {
+        gotoYTButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"));
@@ -105,13 +95,13 @@ public class LoginActivity extends Activity {
     }
 
     private void sendLoginNotification() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(LoginActivity.this,"Witaj w pizzerii!");
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(LoginActivity.this, "Witaj w pizzerii!");
         builder.setContentTitle("Witaj w pizzerii!");
         builder.setContentText("Życzymy Ci jak najsmaczniejszych chwil!:)");
         builder.setSmallIcon(R.drawable.ic_launcher_background);
         builder.setAutoCancel(true);
 
         NotificationManagerCompat managerCompat = NotificationManagerCompat.from(LoginActivity.this);
-        managerCompat.notify(1,builder.build());
+        managerCompat.notify(1, builder.build());
     }
 }
